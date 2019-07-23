822ba791b64afc49b3662b2226b3e51896754089
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-07-23 21:03:43 +0900

    コメント追加

--- app.rb
+++ app.rb
@@ -6,11 +6,13 @@ get '/' do
 end
 
 get '/hello' do
+    # query string から取得
     name = params[:name]
     "<h1>Hello #{name}!</h1>"
 end
 
 get '/user/:user_name' do
+    # 可変URLからuser_nameを取得
     user_name = params[:user_name]
     "<h1>User: #{user_name}!</h1>"
 end
